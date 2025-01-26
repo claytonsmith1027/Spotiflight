5525c8bc4a5843d432fb74277516eadbf79c51ec
walk3rc <walk3rc04@gmail.com>
2025-01-25 20:12:53 -0600

    Very cool working random playlist maker

--- src/script.js
+++ src/script.js
@@ -7,18 +7,26 @@ if (!code) {
 } else {
     const accessToken = await getAccessToken(clientId, code);
     const profile = await fetchProfile(accessToken);
-    populateUI(profile);
+    //populateUI(profile);
     try{
-        const topSongs = await getTopSongs(accessToken, 1);
-        console.log("Got top songs:", topSongs);
-        console.log(topSongs.items);
-        const recResults = await getRecommendations(topSongs.items, accessToken, 30);
-        // const playlist = await createPlaylist("Test1", profile, accessToken);
-        // const formattedSongArray = createSongListFromTime(6000000, topSongs.items);
-        // const addSongResult = await addSongs(playlist, formattedSongArray, accessToken);
+        // const topSongs = await getTopSongs(accessToken, 1);
+        // console.log("Got top songs:", topSongs);
+        
+        // const newReleaseResults = await getNewAlbums(20,0,accessToken);
+        // console.log("Got new release results", newReleaseResults);
+        // for (const album of newReleaseResults.albums.items){
+        //     console.log(album.name);
+        //     const songsInAlbum = await getSongsFromAlbum(album, accessToken);
+        //     console.log(songsInAlbum);
+        // }
+
+        const playlist = await createPlaylist("Test1", profile, accessToken); // Creates an empty playlist
+        const formattedSongArray = await createSongListFromTime(12000000, accessToken); // Creates an array of formatted strings with songs adding to given time
+        const addSongResult = await addSongs(playlist, formattedSongArray, accessToken); // adds songs to playlist
+        
     }
     catch (error) {
-        console.error("Failed to get top songs:", error);
+        console.error("Failed main program: ", error);
     }
     // try{
     //     const newPlaylist = await createPlaylist("TEST", profile, accessToken);
@@ -93,21 +101,21 @@ async function fetchProfile(token) {
     return await result.json();
 }
 
-function populateUI(profile) {
-    document.getElementById("displayName").innerText = profile.display_name;
-    if (profile.images[0]) {
-        const profileImage = new Image(200, 200);
-        profileImage.src = profile.images[0].url;
-        document.getElementById("avatar").appendChild(profileImage);
-        document.getElementById("imgUrl").innerText = profile.images[0].url;
-    }
-    document.getElementById("id").innerText = profile.id;
-    document.getElementById("email").innerText = profile.email;
-    document.getElementById("uri").innerText = profile.uri;
-    document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
-    document.getElementById("url").innerText = profile.href;
-    document.getElementById("url").setAttribute("href", profile.href);
-}
+// function populateUI(profile) {
+//     document.getElementById("displayName").innerText = profile.display_name;
+//     if (profile.images[0]) {
+//         const profileImage = new Image(200, 200);
+//         profileImage.src = profile.images[0].url;
+//         document.getElementById("avatar").appendChild(profileImage);
+//         document.getElementById("imgUrl").innerText = profile.images[0].url;
+//     }
+//     document.getElementById("id").innerText = profile.id;
+//     document.getElementById("email").innerText = profile.email;
+//     document.getElementById("uri").innerText = profile.uri;
+//     document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+//     document.getElementById("url").innerText = profile.href;
+//     document.getElementById("url").setAttribute("href", profile.href);
+// }
 
 async function createPlaylist(playListName, profile, token) {
     try {
@@ -128,7 +136,6 @@ async function createPlaylist(playListName, profile, token) {
       }
   
       const playlist = await playlistResult.json();
-      console.log(playlist);
       return playlist;
     } catch (error) {
       console.error("Error creating playlist:", error);
@@ -142,10 +149,6 @@ async function createPlaylist(playListName, profile, token) {
 
   async function addSongs(playlist, formattedSongsArray, token){
     try {
-        console.log(formattedSongsArray);
-        console.log("Trying to add songs");
-        console.log("playlist id: " + playlist.id);
-        console.log(formattedSongsArray.join(','));
         if(formattedSongsArray.length == 0){
             throw new Error('No songs');
         }
@@ -195,38 +198,9 @@ async function createPlaylist(playListName, profile, token) {
   }
 
 
-// async function getRecommendations(tracks, token,count){
-//     var seedArray = [];
-//     console.log(tracks);
-//     for (const track of tracks){
-//         seedArray.push(track.id);
-//     }
-//     try{
-//         console.log("Trying to get rec with: ", seedArray.join(','));
-//         const recResults = await fetch(`https://api.spotify.com/v1/recommendations?seed_tracks=${seedArray.join('%2C')}&limit=${count}`, {
-//             method: "GET",
-//             headers: {
-//               "Content-Type": "application/json",
-//               Authorization: `Bearer ${token}`
-//             }
-//           });
-
-//           if (!recResults.ok){
-//             throw new Error(`HTTP erro! status : ${recResults.status}`);
-//           }
-
-//           const recommendations = await recResults.json();
-//           console.log("Recs: ", recommendations);
-//           return recommendations;
-//     } catch (error){
-//         console.error("Error getting recommendations", error);
-//         throw error;
-//     }
-// }
-
-async function getNewReleases(limit, offset){
+async function getNewAlbums(limit, offset, token){
     try{
-        const newResults = await fetch(`https://api.spotify.com/v1/browse/new-releases?limit=${limit}&offset=${offset}`, {
+        const newResultsAlbums = await fetch(`https://api.spotify.com/v1/browse/new-releases?limit=${limit}&offset=${offset}`, {
             method: "GET",
             headers: {
               "Content-Type": "application/json",
@@ -234,29 +208,74 @@ async function getNewReleases(limit, offset){
             }
           });
 
-          if (!newResults.ok){
-            throw new Error(`HTTP error status : ${newResults.status}`);
+          if (!newResultsAlbums.ok){
+            throw new Error(`HTTP error status : ${newResultsAlbums.status}`);
           }
 
-          const newReleases = await newResults.json();
-          console.logs("New releases: ", newReleases);
+          const newReleases = await newResultsAlbums.json();
           return newReleases;
     }catch (error){
-        console.log("error getting new releases", error);
+        console.log("error getting new releases albums", error);
     }
 }
 
-function createSongListFromTime(timeGoal, tracks){
+async function getSongsFromAlbum(album, token){
+    try{
+        const songResults = await fetch(`https://api.spotify.com/v1/albums/${album.id}/tracks`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`
+            }
+          });
+
+          if (!songResults.ok){
+            throw new Error(`HTTP error status : ${songResults.status}`);
+          }
+
+          const songs = await songResults.json();
+          return songs;
+    }catch (error){
+        console.log("error getting songs from album", error);
+    }
+}
+
+function random(min, max){
+    return Math.ceil(Math.random() * (max - min) + min);
+}
+
+function fisherYatesShuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+async function createSongListFromTime(timeGoal, token){
     var addedTracks = [];
     var time = 0;
+    var releaseOffest = 0 + random(0,10);
+    const requestAlmbumCount = 20;
+    const albumSongLimit = 4;
 
-    for (const track of tracks) {
-        addedTracks.push(formatSongString(track.id));
-        time += track.duration_ms;
-        if (time > timeGoal){break;}
+    while (time <= timeGoal){
+        const albums = await getNewAlbums(requestAlmbumCount, releaseOffest, token);
+        for (const album of albums.albums.items){
+            const tracks = await getSongsFromAlbum(album, token);
+            var albumSongCount = 0;
+            for (const track of tracks.items){
+                if (time > timeGoal){break;}
+                if (albumSongCount == albumSongLimit){break;}
+                addedTracks.push(formatSongString(track.id));
+                time += track.duration_ms;
+                albumSongCount += 1;
+            }
+            if (time > timeGoal){break;}
+        }
+        releaseOffest += requestAlmbumCount + random(0,5);
     }
-
-    addedTracks.concat(addedTracks);
-    return addedTracks;
+    const shuffled = fisherYatesShuffle(addedTracks)
+    return shuffled;
 }
 
